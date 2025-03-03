92bd5ddb04f1e4e116516c9dcc51023d87e85ce7
Syifa Revalina Kamila <cipakamila19@gmail.com>
2025-03-03 20:48:06 +0700

    Praktikum 6

--- Minggu-3/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu-3/PWL_POS/app/Http/Controllers/UserController.php
@@ -2,21 +2,37 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\UserModel;
 use Illuminate\Http\Request;
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
+
 
 class UserController extends Controller
 {
-    public function index()
-    {
-        // tambah data user dengan Eloquent Model
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data); // update data user
+    public function index() {
+        // //coba akses usermodel
+        // $user = UserModel::all(); //ambil semua dari tabel m_user
+        // return view('user', ['data' => $user]);
 
-        // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        //tambah data user dengan eloquent model
+        // $data = [
+        //     'username' => 'customer-1',
+        //     'nama' => 'Pelanggan',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 4
+        // ];
+        // UserModel::insert($data);
+
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
+
+        $data = [
+            'nama' => "Pelanggan Pertama",
+        ];
+        UserModel::where('username', 'customer-1')->update($data);
+
+        $user = UserModel::all();
         return view('user', ['data' => $user]);
+
     }
 }
