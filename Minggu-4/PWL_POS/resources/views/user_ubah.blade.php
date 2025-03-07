0674a97986512fdca24c39384858929f481fbd3f
Syifa Revalina Kamila <cipakamila19@gmail.com>
2025-03-08 01:10:17 +0700

    Praktikum 2.6 – Create, Read, Update, Delete (CRUD)

--- Minggu-4/PWL_POS/resources/views/user_ubah.blade.php
+++ Minggu-4/PWL_POS/resources/views/user_ubah.blade.php
@@ -3,20 +3,20 @@
     <a href="/user">Kembali</a>
     <br><br>
 
-    <form method="post" action="/user/ubah_simpan/{{ $data->user_id }}">
+    <form method="post" action="/user/ubah_simpan/{{ $data->id }}">
         {{ csrf_field() }}
-        {{ method_field("PUT") }}
+        {{ method_field('PUT') }}
 
         <label>Username</label>
         <input type="text" name="username" placeholder="Masukan Username" value="{{ $data->username }}">
         <br>
 
         <label>Nama</label>
-        <input type="text" name="nama" placeholder="Masukan Nama" value="{{ $data->username }}">
+        <input type="text" name="nama" placeholder="Masukan Nama" value="{{ $data->nama }}">
         <br>
 
         <label>Password</label>
-        <input type="password" name="password" placeholder="Masukan Password" value="{{ $data->password }}">
+        <input type="password" name="password" placeholder="Masukan Password (kosongkan jika tidak ingin mengubah)">
         <br>
 
         <label>Level ID</label>
