792ddca69f3cd10bd3d2b4170dce82a375f4997b
Syifa Revalina Kamila <cipakamila19@gmail.com>
2025-03-07 11:05:40 +0700

    Jobsheet 4

---
+++ Minggu-4/PWL_POS/app/Models/UserModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+    /**
+     * the attributes that are mass assignable
+     * 
+     * @var array
+     */
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+}
