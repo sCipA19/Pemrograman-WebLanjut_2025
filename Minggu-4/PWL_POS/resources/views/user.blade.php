1f360238d72e2de1e17d8e1d23a80e2cbddfcd0f
Syifa Revalina Kamila <cipakamila19@gmail.com>
2025-03-07 13:58:59 +0700

    Buat jumlah script

--- Minggu-4/PWL_POS/resources/views/user.blade.php
+++ Minggu-4/PWL_POS/resources/views/user.blade.php
@@ -1,25 +1,33 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data User</title>
 </head>
 <body>
     <h1>Data User</h1>
-    <table border="1" cellpadding="2" cellspacing="0">
+    <table border="1">
         <tr>
-            <th>ID</th>
+            <th>Jumlah Pengguna</th>
+            {{-- <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
-            <th>ID Level Pengguna</th>
+            <th>ID Level Pengguna</th> --}}
         </tr>
+        {{-- @foreach ($data as $user) --}}
             <tr>
-                <td>{{ $data->user_id }}</td>
+                <td>{{ $jumlahUser }}</td>
+                {{-- <td>{{ $user->user_id }}</td>
+                <td>{{ $user->username }}</td>
+                <td>{{ $user->nama }}</td>
+                <td>{{ $user->level_id }}</td> --}}
+                {{-- <td>{{ $data->user_id }}</td>
                 <td>{{ $data->username }}</td>
                 <td>{{ $data->nama }}</td>
-                <td>{{ $data->level_id }}</td>
+                <td>{{ $data->level_id }}</td> --}}
             </tr>
-        @else
-        @endif
+        {{-- @endforeach --}}
     </table>
 </body>
 </html>
