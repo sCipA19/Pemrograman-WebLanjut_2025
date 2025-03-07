804252a435894bcd24f0a81a837d66fb97669d22
Syifa Revalina Kamila <cipakamila19@gmail.com>
2025-03-07 13:40:57 +0700

    Retreiving Aggregrates

--- Minggu-4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu-4/PWL_POS/app/Http/Controllers/UserController.php
@@ -10,7 +10,8 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = UserModel::where('username', 'manager9')->firstOrFail();
-        return view('user', ['data' => $users]);
+        $user = UserModel::where('level_id',2)->count();
+        dd($user);
+        return view('user', ['data' => $user]);
     }
 }
