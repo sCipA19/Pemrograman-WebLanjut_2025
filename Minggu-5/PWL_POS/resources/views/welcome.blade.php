421a6d459f83dced2c532d3c63f75397eb93ab34
Syifa Revalina Kamila <cipakamila19@gmail.com>
2025-03-14 10:59:27 +0700

    Jobsheet 5

---
+++ Minggu-5/PWL_POS/resources/views/welcome.blade.php
@@ -0,0 +1,35 @@
+{{-- <!DOCTYPE html>
+ <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+     <head>
+         <meta charset="utf-8">
+ @@ -130,4 +130,30 @@
+             </div>
+         </div>
+     </body>
+ </html> --}}
+ @extends('layout.app')
+ 
+ {{-- Customize layout sections --}}
+ 
+ @section('subtitle', 'Welcome')
+ @section('content_header_title', 'Home')
+ @section('content_header_subtitle', 'Welcome')
+ 
+ {{-- Content body: main page content --}}
+ 
+ @section('content_body')
+     <p>Welcome to this beautiful admin panel.</p>
+ @stop
+ 
+ {{-- Push extra CSS --}}
+ 
+ @push('css')
+     {{-- Add here extra stylesheets --}}
+     {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+ @endpush
+ 
+ {{-- Push extra scripts --}}
+ 
+ @push('js')
+     <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+ @endpush
