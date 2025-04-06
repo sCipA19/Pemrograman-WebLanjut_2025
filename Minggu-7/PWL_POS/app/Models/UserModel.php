a054e1b93d5b612590e2c3dfe0c37e03c195387a
Syifa Revalina Kamila <cipakamila19@gmail.com>
2025-04-06 19:52:18 +0700

    Praktikum 1 – Implementasi Authentication & tugas 1

---
+++ Minggu-7/PWL_POS/app/Models/UserModel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UserModel extends Authenticatable
+{
+    use HasFactory;
+
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+    protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at'];
+
+    protected $hidden = ['password'];
+    protected $casts = ['password' => 'hashed'];
+
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    // Tambahan: gunakan kolom username untuk login
+    public function getAuthIdentifierName()
+    {
+        return 'username';
+    }
+}
