04a4fb80cd6cbab4e976282d844382622aa7ac1d
Syifa Revalina Kamila <cipakamila19@gmail.com>
2025-04-22 20:57:02 +0700

    Implementasikan CRUD API  m_barang

--- Minggu-10/PWL_POS/routes/api.php
+++ Minggu-10/PWL_POS/routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Api\LogoutController;
 use App\Http\Controllers\Api\LevelController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\KategoriController;
+use App\Http\Controllers\Api\BarangController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -43,3 +44,9 @@ Route::get('kategoris', [KategoriController::class, 'index']);
  Route::get('kategoris/{kategori}', [KategoriController::class, 'show']); 
  Route::put('kategoris/{kategori}', [KategoriController::class, 'update']); 
  Route::delete('kategoris/{kategori}', [KategoriController::class, 'destroy']);
+
+ Route::get('barangs', [BarangController::class, 'index']);
+ Route::post('barangs', [BarangController::class, 'store']);
+ Route::get('barangs/{barang}', [BarangController::class, 'show']);
+ Route::put('barangs/{barang}', [BarangController::class, 'update']);
+ Route::delete('barangs/{barang}', [BarangController::class, 'destroy']);
